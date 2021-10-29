f7f2e3d0b9792e58b593e2cb2560a449aacb4a69
Chase-Stokes <chasehstokes@gmail.com>
2021-10-29 09:21:48 -0700

    first test complete; second test for inputted 1 to return beep

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 function beepBoop(input) {
   let beepBoopArray = [];
-  for (let i=0; input.length; i++){
-    if (word.includes)
-  }
+  for (let i=0; i < input.length; i++){
+    if (input.includes === "3") {
+      beepBoopArray.push("Wont you be my Neighbor");
+    }else if (input.includes === "2") {
+      beepBoopArray.push("boop");
+    }else if (input.includes === "1") {
+      beepBoopArray.push("beep");
+    }else {
+      beepBoopArray.push(i)
+    }
+  } 
+  return beepBoopArray;
 }
